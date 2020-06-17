fa1bdd5a5bcc4fee5de42681ce8c981b2fdb66dc
orest-2 <orest988@ukr.net>
2020-06-17 18:30:39 +0300

    feat: add fw-book demo

--- packages/examples/src/fw-book-example.vue
+++ packages/examples/src/fw-book-example.vue
@@ -1,7 +1,6 @@
 <template>
-  <div>
+  <div class="bb-custom-wrapper">
     <fw-book
-      class="book"
       :data="pages"
       :startPageIndex="1"
       :endPageIndex="pages.length - 2"
@@ -12,9 +11,8 @@
       :autoNextPageDelayTime="1000"
       :loop="loop"
     >
-      <template v-slot:page="page">
-        <img :src="page.url" />
-        <p>{{ page.title }}</p>
+      <template v-slot:page="{ page, index }">
+        <img :src="page.src" :alt="`image${index}`" />
       </template>
     </fw-book>
   </div>
@@ -27,40 +25,11 @@ export default {
       autoNextPage: true,
       loop: false,
       pages: [
-        {
-          url: "https://wangzongxu.github.io/img-cache/book/book-page-1.jpg",
-          title: ""
-        },
-        {
-          url: "https://wangzongxu.github.io/img-cache/book/book-page-2.jpg",
-          title: ""
-        },
-        {
-          url: "https://wangzongxu.github.io/img-cache/book/book-page-3.jpg",
-          title:
-            "The lake was home to many animals.↵Frogs and turtles were living in the pond."
-        },
-        {
-          url: "https://wangzongxu.github.io/img-cache/book/book-page-4.jpg",
-          title:
-            "Some turtles were resting on logs.↵Some frogs were waiting on water lilies for flies to catch."
-        },
-        {
-          url: "https://wangzongxu.github.io/img-cache/book/book-page-5.jpg",
-          title: "Two, small ears were moving slowly in the water."
-        },
-        {
-          url: "https://wangzongxu.github.io/img-cache/book/book-page-6.jpg",
-          title: "All of a sudden a huge mouth appeared with big tusks."
-        },
-        {
-          url: "https://wangzongxu.github.io/img-cache/book/book-page-7.jpg",
-          title: "A hippo was coming up for air.↵Her calf was coming up, too."
-        },
-        {
-          url: "https://wangzongxu.github.io/img-cache/book/book-page-8.jpeg",
-          title: ""
-        }
+        { src: "/images/bookblock/1.jpg" },
+        { src: "/images/bookblock/2.jpg" },
+        { src: "/images/bookblock/3.jpg" },
+        { src: "/images/bookblock/4.jpg" },
+        { src: "/images/bookblock/5.jpg" }
       ]
     };
   },
@@ -71,21 +40,22 @@ export default {
 };
 </script>
 
-<style scoped>
-.book {
+<style lang="scss">
+@import "./styles/demo1.scss";
+.fw-book {
+  width: 420px;
+  border-radius: 10px;
   position: relative;
-  margin: 10% auto 0;
+  perspective: 1500px;
 }
-.book img {
-  width: 100%;
+.fw-book .fw-bookitem {
+  width: 50%;
   height: 100%;
-}
-.book p {
+  border-radius: 10px;
+  background: #ffffff;
+  overflow: hidden;
   position: absolute;
-  bottom: 0;
-  width: 100%;
-  text-align: center;
-  height: 50px;
-  font-size: 10px;
+  top: 0;
+  right: 0;
 }
 </style>
